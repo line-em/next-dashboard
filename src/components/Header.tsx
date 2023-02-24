db7fc58e473278590f97190474e8db7dc2ff081e
Line-em <alineemily@hotmail.com>
2023-02-24 11:48:29 +0000

    change title depending on page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { useState } from "react";
 import { BsFillHouseDoorFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 const Header = () => {
+	const { asPath } = useRouter();
+	console.log(asPath);
+
 	const names: Array<string> = [
 		"Basil",
 		"Cedric",
@@ -22,10 +26,16 @@ const Header = () => {
 
 	return (
 		<header className="flex justify-between items-center px-4 py-3 bg-purple-200 text-purple-800">
-			<h2 className="hidden sm:flex">Dashboard</h2>
+			<h2 className="font-bold">
+				{asPath === "/customers"
+					? "Customers"
+					: asPath === "/recent"
+					? "Last Orders"
+					: "Dashboard"}
+			</h2>
 			<div className="flex items-center gap-2 flex-wrap ml-auto">
-				<h2>
-					Welcome Back, <strong>{currentAdmin}</strong>
+				<h2 className="hidden sm:block">
+					Welcome Back,<strong>{" " + currentAdmin}</strong>
 				</h2>
 				<Link href={"/"}>
 					<BsFillHouseDoorFill className="cursor-pointer" />
